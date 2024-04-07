c664919db1a7855f99d31ad639399774709a52bb
Erwin Darsono <erwindarsono08@gmail.com>
2024-04-07 23:09:16 +0800

    initial setup for tailwind and react

---
+++ rts/src/App.jsx
@@ -0,0 +1,9 @@
+import './stylesheets/output.css'
+
+export default function App(){
+  return (
+    <div className="bg-sky-700">
+      <p className="text-xl">Hello world!</p>
+    </div>
+  )
+}
---
+++ rts/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+import colors from 'tailwindcss/colors'
+
+export default {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    colors: {
+      sky: colors.sky
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
